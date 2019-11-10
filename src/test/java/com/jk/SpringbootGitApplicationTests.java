910e54c00feca933547b7b1e03433af3a8e3ab85
Your Name <859964416@qq.com>
2019-11-10 15:12:08 +0800

    a提交

--- src/test/java/com/jk/SpringbootGitApplicationTests.java
+++ src/test/java/com/jk/SpringbootGitApplicationTests.java
@@ -13,6 +13,7 @@ public class SpringbootGitApplicationTests {
     public void contextLoads() {
         System.out.println(1111);
         System.out.println(2222);
+        System.out.println(3333);
     }
 
 }
